a251219fd31f5f8d5c07bc0bb8a21a551c26b786
nbarne8 <nbarne8@lsu.edu>
2016-02-25 09:01:51 -0600

    Added files via upload

---
+++ demo/js/script.js
@@ -0,0 +1,39 @@
+	var map = L.map('map').setView([30, -90], 4);
+
+	var streets = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	var Lightning = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer", {
+    layers: '1',
+    format: 'image/png',
+    transparent: true,
+    attribution: "NOAA"
+	}).addTo(map);
+
+	var Humidity = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_relhumidity_offsets/MapServer/WMSServer", {
+		layers: '1',
+		format: 'image/png',
+		transparent: true,
+		attribution: "NOAA",
+		opacity: 0.25
+	}).addTo(map);
+
+	var Precipitation = L.tileLayer.wms("https://idpgis.ncep.noaa.gov/arcgis/services/NWS_Observations/radar_base_reflectivity/MapServer/WMSServer", {
+		layers: '1',
+		format: 'image/png',
+		transparent: true,
+		attribution: "NOAA",
+	}).addTo(map);
+
+	var baseLayers = {
+	    "Streets": streets
+	};
+
+	var overlays = {
+	    "Lightning": Lightning,
+	    "Humidity": Humidity,
+			"Precipitation": Precipitation
+	};
+
+	L.control.layers(baseLayers, overlays).addTo(map);
